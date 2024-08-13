4bf02e9d941fff5ab85cbf19cd78ee231f9505b5
Seamus Finlayson <77430559+SeamusFinlayson@users.noreply.github.com>
2024-08-12 18:18:07 -0700

    Version 1.2.2 - Only display background shape for dark user colors

--- src/rulerBuilder.ts
+++ src/rulerBuilder.ts
@@ -7,6 +7,7 @@ import {
 } from "@owlbear-rodeo/sdk";
 import { getLabelPosition, calculateDisplayDistance } from "./mathHelpers";
 import { Grid, Player, RulerIds } from "./types";
+import parse from "color-parse";
 
 export async function buildRuler(
   rulerIds: RulerIds,
@@ -19,66 +20,85 @@ export async function buildRuler(
 ): Promise<Item[]> {
   const points = [startPosition, pointerPosition];
 
-  const rulerBackground = buildCurve()
-    .id(rulerIds.background)
-    .attachedTo(rulerIds.label)
-    .points(points)
-    .strokeColor("white")
-    .strokeOpacity(0.2)
-    .fillOpacity(0)
-    .strokeWidth(grid.dpi / 8)
-    .tension(0)
-    .visible(visible)
-    .layer("RULER")
-    .zIndex(10000)
-    .build();
+  const rulerConstituentItems: any[] = [];
 
-  const rulerLine = buildCurve()
-    .id(rulerIds.line)
-    .attachedTo(rulerIds.background)
-    .points(points)
-    .strokeColor(player.color)
-    .fillOpacity(0)
-    .strokeWidth(grid.dpi / 15)
-    .strokeDash([grid.dpi / 3, grid.dpi / 5])
-    .tension(0)
-    .visible(visible)
-    .layer("RULER")
-    .zIndex(10002)
-    .build();
+  rulerConstituentItems.push(
+    buildCurve()
+      .id(rulerIds.line)
+      .attachedTo(rulerIds.label)
+      .points(points)
+      .strokeColor(player.color)
+      .fillOpacity(0)
+      .strokeWidth(grid.dpi / 15)
+      .strokeDash([grid.dpi / 3, grid.dpi / 5])
+      .tension(0)
+      .visible(visible)
+      .layer("RULER")
+      .zIndex(10002)
+      .build()
+  );
 
-  const rulerLabel = buildLabel()
-    .id(rulerIds.label)
-    .attachedTo(rulerIds.line)
-    .position(getLabelPosition(grid, pointerPosition))
-    .plainText(await calculateDisplayDistance(grid, points))
-    // .fontFamily("Times New Roman")
-    .pointerHeight(0)
-    .backgroundOpacity(0.7)
-    .visible(visible)
-    .layer("RULER")
-    .zIndex(10004)
-    .build();
+  rulerConstituentItems.push(
+    buildLabel()
+      .id(rulerIds.label)
+      .attachedTo(rulerIds.line)
+      .position(getLabelPosition(grid, pointerPosition))
+      .plainText(await calculateDisplayDistance(grid, points))
+      // .fontFamily("Times New Roman")
+      .pointerHeight(0)
+      .backgroundOpacity(0.7)
+      .visible(visible)
+      .layer("RULER")
+      .zIndex(10004)
+      .build()
+  );
 
-  if (!endDot) {
-    return [rulerBackground, rulerLine, rulerLabel];
+  if (endDot) {
+    rulerConstituentItems.push(
+      buildShape()
+        .id(rulerIds.endDot)
+        .attachedTo(rulerIds.label)
+        .layer("RULER")
+        .position(pointerPosition)
+        .shapeType("CIRCLE")
+        .fillColor(player.color)
+        .strokeColor("white")
+        .strokeOpacity(0.03)
+        .strokeWidth(grid.dpi / 120)
+        .width(grid.dpi / 4)
+        .height(grid.dpi / 4)
+        .zIndex(10001)
+        .disableHit(true)
+        .build()
+    );
   }
 
-  const rulerEndDot = buildShape()
-    .id(rulerIds.endDot)
-    .attachedTo(rulerIds.label)
-    .layer("RULER")
-    .position(pointerPosition)
-    .shapeType("CIRCLE")
-    .fillColor(player.color)
-    .strokeColor("white")
-    .strokeOpacity(0.03)
-    .strokeWidth(grid.dpi / 120)
-    .width(grid.dpi / 4)
-    .height(grid.dpi / 4)
-    .zIndex(10001)
-    .disableHit(true)
-    .build();
+  if (useBackground(player.color)) {
+    rulerConstituentItems.push(
+      buildCurve()
+        .id(rulerIds.background)
+        .attachedTo(rulerIds.label)
+        .points(points)
+        .strokeColor("white")
+        .strokeOpacity(0.2)
+        .fillOpacity(0)
+        .strokeWidth(grid.dpi / 8)
+        .tension(0)
+        .visible(visible)
+        .layer("RULER")
+        .zIndex(10000)
+        .disableHit(true)
+        .build()
+    );
+  }
 
-  return [rulerBackground, rulerLine, rulerLabel, rulerEndDot];
+  return rulerConstituentItems;
 }
+
+const useBackground = (color: string): boolean => {
+  const parsedColor = parse(color);
+  if (parsedColor.space !== "rgb") return false;
+  const colorMax = Math.max(...parsedColor.values);
+  if (colorMax > 100) return false;
+  return true;
+};
